f028bf9895a64dd20448399639fd369a21c0af71
Logesh-Kaavyan <logeshkaavyan.pks2022@vitstudent.ac.in>
2025-10-15 20:53:13 +0530

    URL change

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,21 +1,22 @@
 // utils/api.js
+const BASE_URL = "https://smart-recipe-generator1.onrender.com";
 export async function getRecipes(ingredients, filters = {}) {
   const params = new URLSearchParams();
   params.append("ingredients", ingredients);
   if (filters.difficulty) params.append("difficulty", filters.difficulty);
   if (filters.maxTime && filters.maxTime !== 60) params.append("maxTime", filters.maxTime);
   if (filters.dietary && filters.dietary.length > 0) params.append("dietary", filters.dietary.join(","));
-  const res = await fetch(`http://127.0.0.1:8000/recipes/?${params.toString()}`);
+  const res = await fetch(`${BASE_URL}/recipes/?${params.toString()}`);
   return res.json();
 }
 
 export async function getSuggestions(query) {
   if (!query) return { suggestions: [] };
-  const res = await fetch(`http://127.0.0.1:8000/suggestions/?query=${query}`);
+  const res = await fetch(`${BASE_URL}/suggestions/?query=${query}`);
   return res.json();
 }
 
 export async function getRecipe(name) {
-  const res = await fetch(`http://127.0.0.1:8000/recipe/${name}`);
+  const res = await fetch(`${BASE_URL}/recipe/${name}`);
   return res.json();
 }
